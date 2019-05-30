83a8ef2f6023c6bc8a70e8e84a36791f36c887ae
Gergan Zhekov (zhekovg) <zhekovg@coventry.ac.uk>
2019-05-30 14:51:50 +0100

    PEP8

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #Gergan-Zhekov-employees
+
+This code finds the pair that has worked together for the longest time period. It was developed in PyCharm, using 
+a standard Python 3.6 interpreted, and was styled according to PEP8, which is the most famous Python coding
+convention.
+
+NOTE: pandas package was used and may need to be installed separately in order to test this code (most of the 
+time it comes included with the main Python packages)
--- employees.py
+++ employees.py
@@ -1,40 +1,64 @@
+"""Pandas is used to open the data text file and read it as a CSV (a plain
+text format used for data management,which uses comma as a separator between
+the different values). The data is exported to a pandas DataFrame, the dates
+are properly formatted from the reader and the NULL value is automatically
+replace with the current date in the same format. The new table is then
+outputted to a new CSV file, and is opened and read by Python. A defaultdict
+is used instead of a normal dictionary, and ProjectID serves as a key.
+Another empty dictionary is created that is going to be filled with the
+results from the loop and contain a pair of employees as a key, and the days
+worked as a value. In the final print statement the code will return the
+pair with the max value
+"""
+
+
+import csv
+import pandas as pd
+
 from collections import defaultdict
 from itertools import combinations
 from datetime import datetime
-import pandas as pd
-import csv
 
-df = pd.read_csv('data.csv') #reading the text data file with pandas
-df['DateTo'] = df['DateTo'].fillna(datetime.today().strftime('%Y-%m-%d')) #replacing NULL with the value of Today
-df.set_index("EmpID", inplace = True) #creating EmpID as the index, as otherwise there would be problem with the reading of the data
-df.to_csv('new_data.csv') #exporting to a new text file
+#global variables
+d = defaultdict(list)
+t = defaultdict(list)
 
-d = defaultdict(list) #
+list_projects = [] #a list that will keep track of days worked on a project
+
+df = pd.read_csv('data.txt')  # reading the text data file with pandas
+
+# replacing NULL with the value of today
+df['DateTo'] = df['DateTo'].fillna(datetime.today().strftime('%Y-%m-%d'))
+
+df.set_index("EmpID", inplace=True)  # creating EmpID as the index
+df.to_csv('new_data.csv')  # exporting to a new text file
+
 
 with open("new_data.csv") as f:
-    next(f) # skip header
+    next(f)  # skip header
     r = csv.reader(f)
-    # unpack use projectID as key and  append empID, start date, finish date
+    # unpack, use projectID as key and  append empID, start date, finish date
     for EmpID, ProjectID, FromDate, ToDate in r:
         d[int(ProjectID)].append((EmpID, FromDate, ToDate))
 
-list_projects = []
-t = defaultdict(list)
 
 for job, aref in d.items():
-
     if len(aref) >= 2:
         for ref in combinations(aref, 2):
-
             begin = max(map(lambda x: x[1], ref))
             end = min(map(lambda x: x[2], ref))
             delta = datetime.strptime(end, '%Y-%m-%d') \
-                    - datetime.strptime(begin, '%Y-%m-%d')
+                - datetime.strptime(begin, '%Y-%m-%d')
             dd = delta.days
             if dd > 0:
                 list_projects.append(dd)
-                t[ref[0][0]+ ' and ' + ref[1][0]].append(dd)
-                #print('Employees with EmpID:', ref[0][0], 'and', ref[1][0],
-                      #'worked together on a common project (Project ID:', job, ') for a total of', dd, 'days\n')
+                t[ref[0][0] + ' and ' + ref[1][0]].append(dd)
 
-print('The pair that has worked together the longest according to the data file input are employees with ID ' + str(max(t, key=t.get)) + ' with a working time of ' + str(max(list_projects)) + ' days')
+                #  print('Employees with EmpID:', ref[0][0], 'and', ref[1][0],
+                # 'worked together on a common project (Project ID:', job, ') \
+                #  for a total of', dd, 'days\n')
+
+print('The pair that has worked together the longest according to the data' +
+      ' file input are employees with ID ' + str(max(t, key = t.get)) + ' with'
+      + ' a working time of ' + str(max(list_projects)) + ' days.')
+
